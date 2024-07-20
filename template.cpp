28c187222c0923eea75ba9175fc0025381e809f9
mdco-ld <migueldco14@gmail.com>
2024-07-19 21:19:11 -0300

    sieve

--- template.cpp
+++ template.cpp
@@ -485,6 +485,38 @@ constexpr int phi_ct(int n) {
     return result;
 }
 
+struct Sieve {
+    vector<int32_t> lp;
+    vector<int32_t> pr;
+
+	Sieve() {}
+
+	Sieve(int n) {
+		setup(n);
+	}
+
+    void setup(int n) {
+        lp.resize(n + 1);
+        pr.clear();
+        for (int i = 2; i <= n; i++) {
+            if (lp[i] == 0) {
+                lp[i] = i;
+                pr.push_back(i);
+            }
+            for (int j = 0; i * pr[j] <= n; j++) {
+                lp[i * pr[j]] = pr[j];
+                if (pr[j] == lp[i]) {
+                    break;
+                }
+            }
+        }
+    }
+
+    inline bool is_prime(int p) { return lp[p] == p; }
+
+    inline int prime(int idx) { return pr[idx]; }
+};
+
 struct intmax {
     int value;
 
