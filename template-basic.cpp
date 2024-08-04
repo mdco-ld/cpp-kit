390789b6f59c77e476a2d558dcb36346c251e3c9
mdco-ld <migueldco14@gmail.com>
2024-08-04 11:10:34 -0300

    template basic

---
+++ template-basic.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+
+#define int ll
+
+const int MOD = 1e9 + 7;
+const int N = 4e5 + 5;
+
+mt19937 rng(random_device{}());
+int n, m, k;
+int arr[N];
+int v[N];
+vector<int> edges[N];
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+	int t;
+	cin>>t;
+	while (t--) {
+	}
+    return 0;
+}
