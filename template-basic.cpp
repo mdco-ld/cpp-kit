1d8061da9ca3e712a4ce71ce35bcd2dd6e039e50
mdco-ld <migueldco14@gmail.com>
2024-08-06 19:19:29 -0300

    dbg

--- template-basic.cpp
+++ template-basic.cpp
@@ -6,6 +6,18 @@ using ll = long long;
 
 #define int ll
 
+void __dbg() { cerr << endl; }
+template <typename T> void __dbg(T t) { cerr << t << endl; }
+template <typename T, typename... TRest> void __dbg(T first, TRest... rest) {
+    cerr << first << ", ";
+    __dbg(rest...);
+}
+#define dbg(...)                                                               \
+    do {                                                                       \
+        cerr << "DBG> " << "(" << #__VA_ARGS__ << ") = ";                      \
+        __dbg(__VA_ARGS__);                                                    \
+    } while (0)
+
 const int MOD = 1e9 + 7;
 const int N = 4e5 + 5;
 
