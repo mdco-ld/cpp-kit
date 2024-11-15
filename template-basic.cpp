67ab2fc9163d54c7461a78e9128cac44e8b75220
mdco-ld <migueldco14@gmail.com>
2024-11-15 12:01:56 -0300

    update template

--- template-basic.cpp
+++ template-basic.cpp
@@ -33,12 +33,8 @@ template <typename T, typename... TRest> T minn(T first, TRest... rest) {
 }
 
 const int MOD = 1e9 + 7;
-const int N = 4e5 + 5;
 
 mt19937 rng(random_device{}());
-int n, m, k, q;
-int v[N];
-vector<int> edges[N];
 
 int32_t main() {
     ios_base::sync_with_stdio(false);
