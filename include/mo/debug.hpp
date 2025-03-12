2178492e60378a5d63e039b73b520a9686985d70
mdco-ld <migueldco14@gmail.com>
2025-03-12 16:57:53 -0300

    debug vectors

--- include/mo/debug.hpp
+++ include/mo/debug.hpp
@@ -2,9 +2,20 @@
 #define _MO_DEBUG_HPP_
 
 #include <iostream>
+#include <vector>
 
 template <typename T> void __dbg2(T t) { std::cerr << t; }
 template <> inline void __dbg2(bool t) { std::cerr << (t ? "true" : "false"); }
+template<typename T> void __dbg2(std::vector<T> &v) {
+	std::cerr << "[";
+	for (int i = 0; i < v.size(); i++) {
+		std::cerr << v[i];
+		if (i + 1 < v.size()) {
+			std::cerr << ", ";
+		}
+	}
+	std::cerr << "]";
+}
 inline void __dbg() { std::cerr << std::endl; }
 template <typename T> void __dbg(T t) {
     __dbg2(t);
