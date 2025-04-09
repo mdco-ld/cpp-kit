970a62d384ac355a91596eb5cda623f6fe707faf
mdco-ld <migueldco14@gmail.com>
2025-04-08 22:01:38 -0300

    add inplace gaussian elimination

--- include/mo/math/gaussian-elimination.hpp
+++ include/mo/math/gaussian-elimination.hpp
@@ -110,6 +110,106 @@ void gaussianElimination(Matrix<F, N, M> &mat,
     }
 }
 
+template <Field F> F::ValueType gaussianElimination(DynMatrix<F> &mat) {
+    using Elem = typename F::ValueType;
+    constexpr auto plus = F::plus;
+    constexpr auto mul = F::mul;
+    constexpr auto neg = F::neg;
+    constexpr auto inv = F::inv;
+
+    Elem ret = F::one();
+
+    int n = mat.rows();
+    int m = mat.cols();
+
+    for (int row = 0, col = 0; col < m && row < n;) {
+        if (mat[row][col] == F::zero()) {
+            for (int i = row + 1; i < n; i++) {
+                if (mat[i][col] != F::zero()) {
+                    std::swap(mat[row], mat[i]);
+                    ret = neg(ret);
+                    break;
+                }
+            }
+            if (mat[row][col] == F::zero()) {
+                col++;
+                continue;
+            }
+        }
+        if (mat[row][col] != F::one()) {
+            Elem pivotInv = inv(mat[row][col]);
+            ret = mul(ret, mat[row][col]);
+            for (int j = col; j < m; j++) {
+                mat[row][j] = mul(mat[row][j], pivotInv);
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (i == row || mat[i][col] == F::zero()) {
+                continue;
+            }
+            Elem q = neg(mat[i][col]);
+            for (int j = col; j < m; j++) {
+                mat[i][j] = plus(mat[i][j], mul(mat[row][j], q));
+            }
+        }
+        row++;
+        col++;
+    }
+    return ret;
+}
+
+template <Field F, int N, int M>
+F::ValueType gaussianElimination(Matrix<F, N, M> &mat) {
+    using Elem = typename F::ValueType;
+    constexpr auto plus = F::plus;
+    constexpr auto mul = F::mul;
+    constexpr auto neg = F::neg;
+    constexpr auto inv = F::inv;
+
+    Elem ret = F::one();
+
+    constexpr int n = mat.rows();
+    constexpr int m = mat.cols();
+
+    for (int row = 0, col = 0; col < m && row < n;) {
+        if (mat[row][col] == F::zero()) {
+            for (int i = row + 1; i < n; i++) {
+                if (mat[i][col] != F::zero()) {
+                    std::array<Elem, n> tmp;
+                    std::copy(mat[i].begin(), mat[i].end(), tmp.begin());
+                    std::copy(mat[row].begin(), mat[row].end(), mat[i].begin());
+                    std::copy(tmp.begin(), tmp.end(), mat[row].begin());
+                    ret = neg(ret);
+                    break;
+                }
+            }
+            if (mat[row][col] == F::zero()) {
+                col++;
+                continue;
+            }
+        }
+        if (mat[row][col] != F::one()) {
+            Elem pivotInv = inv(mat[row][col]);
+            ret = mul(ret, mat[row][col]);
+            for (int j = col; j < m; j++) {
+                mat[row][j] = mul(mat[row][j], pivotInv);
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (i == row || mat[i][col] == F::zero()) {
+                continue;
+            }
+            Elem q = neg(mat[i][col]);
+            for (int j = col; j < m; j++) {
+                mat[i][j] = plus(mat[i][j], mul(mat[row][j], q));
+            }
+        }
+        row++;
+        col++;
+    }
+	return ret;
+}
+
 }; // namespace math
 
 }; // namespace mo
