e4251c67cd8e2f4089cc4f2ea9b420f131a34134
mdco-ld <migueldco14@gmail.com>
2025-05-05 15:40:33 -0300

    HLD

---
+++ include/mo/tree/hld.hpp
@@ -0,0 +1,118 @@
+#ifndef _MO_TREE_HLD_HPP_
+#define _MO_TREE_HLD_HPP_
+
+#include <mo/segtree.hpp>
+#include <mo/tree/tree.hpp>
+
+#include <iostream>
+
+namespace mo {
+
+namespace tree {
+
+template <class T, T (*op)(T, T), T (*e)(), bool oneBased = true> class HLD {
+  public:
+    using Segtree = SegmentTree<T, op, e>;
+
+    HLD(Tree<int, oneBased> &t) : HLD(t, oneBased) {}
+
+    HLD(Tree<int, oneBased> &t, int root)
+        : n(t.size()), root(root), tin(t.size() + oneBased),
+          tout(t.size() + oneBased), nxt(t.size() + oneBased, -1),
+          head(t.size() + oneBased), cnt(t.size() + oneBased, 1),
+          par(t.size() + oneBased), depth(t.size() + oneBased), t(t) {
+        st.build(n);
+        getCnt(root);
+        head[root] = root;
+        prepare(root);
+    }
+
+    void update(int v, T value) { st.update(tin[v], value); }
+
+    void add(int v, T value) { st.add(tin[v], value); }
+
+    T querySubtree(int v) { return st.query(tin[v], tout[v]); }
+
+    T query(int v, int u) {
+        T ans = e();
+        while (head[v] != head[u]) {
+            if (depth[head[v]] < depth[head[u]]) {
+                ans = op(ans, st.query(tin[head[u]], tin[u] + 1));
+                u = par[head[u]];
+            } else {
+                ans = op(ans, st.query(tin[head[v]], tin[v] + 1));
+                v = par[head[v]];
+            }
+        }
+        if (u == v) {
+            ans = op(ans, st.get(tin[v]));
+            return ans;
+        }
+        if (depth[v] > depth[u]) {
+            ans = op(ans, st.query(tin[u], tin[v] + 1));
+        } else {
+            ans = op(ans, st.query(tin[v], tin[u] + 1));
+        }
+        return ans;
+    }
+
+    inline int getRoot() { return root; }
+
+  private:
+    int n;
+    int root;
+    Segtree st;
+    std::vector<int> tin;
+    std::vector<int> tout;
+    std::vector<int> nxt;
+    std::vector<int> head;
+    std::vector<int> cnt;
+    std::vector<int> par;
+    std::vector<int> depth;
+    Tree<int, oneBased> &t;
+    int tm = 0;
+
+    void getCnt(int v, int p = -1) {
+		par[v] = p == -1 ? v : p;
+        for (int u : t[v]) {
+            if (u == p) {
+                continue;
+            }
+            depth[u] = depth[v] + 1;
+            getCnt(u, v);
+            cnt[v] += cnt[u];
+            if (nxt[v] == -1 || cnt[u] > cnt[nxt[v]]) {
+                nxt[v] = u;
+            }
+        }
+    }
+
+    void prepare(int v) {
+        tin[v] = tm++;
+        if (nxt[v] != -1) {
+			head[nxt[v]] = head[v];
+            prepare(nxt[v]);
+        }
+        for (int u : t[v]) {
+            if (u == par[v] || u == nxt[v]) {
+                continue;
+            }
+			head[u] = u;
+            prepare(u);
+        }
+        tout[v] = tm;
+    }
+};
+
+template <class T, T (*op)(T, T), T (*e)(), class U, T (*mapping)(U, T),
+          U (*compose)(U, U), U (*id)()>
+class LazyHLD {
+  public:
+  private:
+};
+
+}; // namespace tree
+
+}; // namespace mo
+
+#endif
--- include/mo/tree/tree.hpp
+++ include/mo/tree/tree.hpp
@@ -2,14 +2,13 @@
 #define _MO_TREE_HPP_
 
 #include <cstdint>
-#include <functional>
 #include <vector>
 
 namespace mo {
 
 namespace tree {
 
-template <typename T, bool oneBased = true> class Tree {
+template <typename T = int, bool oneBased = true> class Tree {
   private:
     std::vector<std::vector<T>> edges;
     int n;
@@ -26,57 +25,6 @@ template <typename T, bool oneBased = true> class Tree {
     const std::vector<T> &operator[](T v) const { return edges[v]; }
 
     inline int64_t size() const { return n; }
-
-    void preOrderDfs(T start, std::function<void(T, T)> visit) {
-        std::function<void(T, T)> dfs_ = [&](T v, T par) {
-            visit(v, par);
-            for (T u : edges[v]) {
-                if (u == par) {
-                    continue;
-                }
-                dfs_(u, v);
-            }
-        };
-        if constexpr (oneBased) {
-            dfs_(start, T{0});
-        } else {
-            dfs_(start, T{n});
-        }
-    }
-
-    void postOrderDfs(T start, std::function<void(T, T)> visit) {
-        std::function<void(T, T)> dfs_ = [&](T v, T par) {
-            for (T u : edges[v]) {
-                if (u == par) {
-                    continue;
-                }
-                dfs_(u, v);
-            }
-            visit(v, par);
-        };
-        if constexpr (oneBased) {
-            dfs_(start, T{0});
-        } else {
-            dfs_(start, T{n});
-        }
-    }
-
-    void dfs(T start, std::function<void(T, T)> visit) {
-        std::function<void(T, T)> dfs_ = [&](T v, T par) {
-            for (T u : edges[v]) {
-                if (u == par) {
-                    continue;
-                }
-                dfs_(u, v);
-                visit(v, u);
-            }
-        };
-        if constexpr (oneBased) {
-            dfs_(start, T{0});
-        } else {
-            dfs_(start, T{n});
-        }
-    }
 };
 
 }; // namespace tree
