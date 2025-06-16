f73552f5e100e85f0eec2ef17f29c96c18676f8c
mdco-ld <migueldco14@gmail.com>
2025-06-16 10:13:03 -0300

    fenwick tree

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # cpp-kit
 
 C++ kit for competitive programming
+
+# Implemented
+- Fenwick Tree
+
+# TODO
+- Li Chao
+- Segment tree
+- FFT
+- Gaussian Elimination
--- include/mo/internal/fenwick-tree.hpp
+++ include/mo/internal/fenwick-tree.hpp
@@ -3,6 +3,7 @@
 
 #include <mo/internal/concepts/group.hpp>
 #include <mo/internal/concepts/monoid.hpp>
+#include <mo/internal/numeric.hpp>
 #include <vector>
 
 namespace mo::internal {
@@ -15,7 +16,7 @@ template <traits::Monoid S> class FenwickTree {
     /**
      * Builds a fenwick tree filled with the neutral element of the group S.
      */
-    FenwickTree(int n) : t(n + 1, S{}), val(n + 1, S{}), n(n) {}
+    FenwickTree(int N) : t(N + 1, S{}), val(N + 1, S{}), n(N) {}
 
     /**
      * changes the value at position `i` to `val`.
@@ -27,12 +28,12 @@ template <traits::Monoid S> class FenwickTree {
     }
 
     /**
-     * Adds the value `val` at position i.
+     * Adds the value `value` at position i.
      */
-    void add(int i, S val) {
-        val[i] += val;
+    void add(int i, S value) {
+        val[i] += value;
         for (; i <= n; i += i & -i) {
-            t[i] += val;
+            t[i] += value;
         }
     }
 
@@ -53,8 +54,48 @@ template <traits::Monoid S> class FenwickTree {
     S sum(int l, int r)
         requires traits::Group<S>
     {
-        return sum(r) - sum(l - 1);
+        return prefixSum(r) - prefixSum(l - 1);
     }
+
+	/**
+	 * Finds the largest prefix that is less than value.
+	 * Uses operator<(S, S).
+	 * `O(log(N))`
+	 */
+	int bisect(S value) {
+		int k = 0;
+		S acc{};
+		for (int i = numeric::bitWidth(n) - 1; i >= 0; i--) {
+			if (k + (1 << i) > n) {
+				continue;
+			}
+			if (acc + t[k + (1 << i)] < value) {
+				k += 1 << i;
+				acc += t[k];
+			}
+		}
+		return k;
+	}
+
+	/**
+	 * Finds largest prefix that satisfies some condition.
+	 * Assumes the prefixes of the tree are partitioned by f at some point.
+	 * `O(log(N))`
+	 */
+	int bisect(auto f) {
+		int k = 0;
+		S acc{};
+		for (int i = numeric::bitWidth(n) - 1; i >= 0; i--) {
+			if (k + (1 << i) > n) {
+				continue;
+			}
+			if (f(acc + t[k + (1 << i)])) {
+				k += 1 << i;
+				acc += t[k];
+			}
+		}
+		return k;
+	}
 
     S operator[](std::size_t i) { return val[i]; }
 
