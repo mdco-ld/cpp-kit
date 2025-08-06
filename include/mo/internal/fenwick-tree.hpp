f69dfb862477141dc09beb5d03ac2eb3c7eb44b3
Miguel <migueldco14@gmail.com>
2025-08-06 15:42:03 -0300

    implement persistent segment tree

--- include/mo/internal/fenwick-tree.hpp
+++ include/mo/internal/fenwick-tree.hpp
@@ -13,96 +13,96 @@ namespace mo::internal {
  */
 template <traits::Monoid S> class FenwickTree {
   public:
-    /**
-     * Builds a fenwick tree filled with the neutral element of the group S.
-     */
-    FenwickTree(int N) : t(N + 1, S{}), val(N + 1, S{}), n(N) {}
+	/**
+	 * Builds a fenwick tree filled with the neutral element of the group S.
+	 */
+	FenwickTree(int N) : t(N + 1, S{}), val(N + 1, S{}), n(N) {}
 
-    /**
-     * changes the value at position `i` to `val`.
-     */
-    void set(int i, S value)
-        requires traits::Group<S>
-    {
-        add(i, value - val[i]);
-    }
+	/**
+	 * changes the value at position `i` to `val`.
+	 */
+	void set(int i, S value)
+		requires traits::Group<S>
+	{
+		add(i, value - val[i]);
+	}
 
-    /**
-     * Adds the value `value` at position i.
-     */
-    void add(int i, S value) {
-        val[i] += value;
-        for (; i <= n; i += i & -i) {
-            t[i] += value;
-        }
-    }
+	/**
+	 * Adds the value `value` at position i.
+	 */
+	void add(int i, S value) {
+		val[i] += value;
+		for (; i <= n; i += i & -i) {
+			t[i] += value;
+		}
+	}
 
-    /**
-     * Sum of values in the prefix `[1, r]`.
-     */
-    S prefixSum(int r) {
-        S s{};
-        for (; r > 0; r -= r & -r) {
-            s += t[r];
-        }
-        return s;
-    }
+	/**
+	 * Sum of values in the prefix `[1, r]`.
+	 */
+	S prefixSum(int r) {
+		S s{};
+		for (; r > 0; r -= r & -r) {
+			s += t[r];
+		}
+		return s;
+	}
 
-    /**
-     * Sum of values in the range `[l, r]`.
-     */
-    S sum(int l, int r)
-        requires traits::Group<S>
-    {
-        return prefixSum(r) - prefixSum(l - 1);
-    }
+	/**
+	 * Sum of values in the range `[l, r]`.
+	 */
+	S sum(int l, int r)
+		requires traits::Group<S>
+	{
+		return prefixSum(r) - prefixSum(l - 1);
+	}
 
-    /**
-     * Finds the largest prefix that is less than value.
-     * Uses operator<(S, S).
-     * `O(log(N))`
-     */
-    int bisect(S value) {
-        int k = 0;
-        S acc{};
-        for (int i = numeric::bitWidth(n) - 1; i >= 0; i--) {
-            if (k + (1 << i) > n) {
-                continue;
-            }
-            if (acc + t[k + (1 << i)] < value) {
-                k += 1 << i;
-                acc += t[k];
-            }
-        }
-        return k;
-    }
+	/**
+	 * Finds the largest prefix that is less than value.
+	 * Uses operator<(S, S).
+	 * `O(log(N))`
+	 */
+	int bisect(S value) {
+		int k = 0;
+		S acc{};
+		for (int i = numeric::bitWidth(n) - 1; i >= 0; i--) {
+			if (k + (1 << i) > n) {
+				continue;
+			}
+			if (acc + t[k + (1 << i)] < value) {
+				k += 1 << i;
+				acc += t[k];
+			}
+		}
+		return k;
+	}
 
-    /**
-     * Finds largest prefix that satisfies some condition.
-     * Assumes the prefixes of the tree are partitioned by f at some point.
-     * `O(log(N))`
-     */
-    int bisect(auto f) {
-        int k = 0;
-        S acc{};
-        for (int i = numeric::bitWidth(n) - 1; i >= 0; i--) {
-            if (k + (1 << i) > n) {
-                continue;
-            }
-            if (f(acc + t[k + (1 << i)])) {
-                k += 1 << i;
-                acc += t[k];
-            }
-        }
-        return k;
-    }
+	/**
+	 * Finds largest prefix that satisfies some condition.
+	 * Assumes the prefixes of the tree are partitioned by f at some point.
+	 * `O(log(N))`
+	 */
+	int bisect(auto pred) {
+		int k = 0;
+		S acc{};
+		for (int i = numeric::bitWidth(n) - 1; i >= 0; i--) {
+			if (k + (1 << i) > n) {
+				continue;
+			}
+			if (pred(acc + t[k + (1 << i)])) {
+				k += 1 << i;
+				acc += t[k];
+			}
+		}
+		return k;
+	}
 
-    S operator[](std::size_t i) { return val[i]; }
+	S operator[](std::size_t i) { return val[i]; }
 
   private:
-    std::vector<S> t;
-    std::vector<S> val;
-    int n;
+	std::vector<S> t;
+	std::vector<S> val;
+	int n;
 };
 
 }; // namespace mo::internal
---
+++ include/mo/internal/persistent-segment-tree.hpp
@@ -0,0 +1,132 @@
+#ifndef _MO_INTERNAL_PERSISTENT_SEGMENT_TREE_HPP_
+#define _MO_INTERNAL_PERSISTENT_SEGMENT_TREE_HPP_
+
+#include <mo/internal/concepts/monoid.hpp>
+#include <vector>
+
+namespace mo::internal {
+
+template <traits::Monoid S> class PersistentSegmentTree {
+	using i64 = long long;
+	i64 L;
+	i64 R;
+	struct Node {
+		S value;
+		Node *left = nullptr;
+		Node *right = nullptr;
+		int t;
+		~Node() {
+			if (left != nullptr && left->t == t) {
+				delete left;
+			}
+			if (right != nullptr && right->t == t) {
+				delete right;
+			}
+		}
+	};
+	std::vector<Node *> root;
+	S getValue(Node *node) {
+		if (node == nullptr) {
+			return S{};
+		}
+		return node->value;
+	}
+	void update(Node *node) {
+		if (node == nullptr) {
+			return;
+		}
+		node->value = getValue(node->left) + getValue(node->right);
+	}
+	Node *set(int t, i64 i, S value, i64 tl, i64 tr, Node *node) {
+		Node *result = new Node();
+		result->t = t;
+		if (node != nullptr) {
+			result->left = node->left;
+			result->right = node->right;
+		}
+		if (tl == i && tr == i + 1) {
+			result->value = value;
+			return result;
+		}
+		i64 tm = tl + (tr - tl) / 2;
+		if (i < tm) {
+			result->left =
+				set(t, i, value, tl, tm, node ? node->left : nullptr);
+		} else {
+			result->right =
+				set(t, i, value, tm, tr, node ? node->right : nullptr);
+		}
+		update(result);
+		return result;
+	}
+	S get(i64 i, i64 tl, i64 tr, Node *&node) {
+		if (node == nullptr) {
+			return S{};
+		}
+		if (tl == i && tr == i + 1) {
+			return node->value;
+		}
+		i64 tm = tl + (tr - tl) / 2;
+		if (i < tm) {
+			return get(i, tl, tm, node->left);
+		} else {
+			return get(i, tm, tr, node->right);
+		}
+	}
+
+	S sum(i64 l, i64 r, i64 tl, i64 tr, Node *&node) {
+		if (node == nullptr) {
+			return S{};
+		}
+		if (tl >= r || tr <= l) {
+			return S{};
+		}
+		if (l <= tl && tr <= r) {
+			return node->value;
+		}
+		i64 tm = tl + (tr - tl) / 2;
+		return sum(l, r, tl, tm, node->left) + sum(l, r, tm, tr, node->right);
+	}
+
+	void bisect(i64 &result, S acc, auto pred, i64 tl, i64 tr, Node *root) {
+		if (root == nullptr) {
+			if (pred(acc)) {
+				result = tl;
+			}
+			return;
+		}
+		i64 tm = tl + (tr - tl) / 2;
+		if (pred(acc + getValue(root->left))) {
+			result = tm - 1;
+			bisect(result, acc, pred, tl, tm, root->left);
+		} else {
+			bisect(result, acc + getValue(root->left), pred, tm, tr,
+				   root->right);
+		}
+	}
+
+  public:
+	PersistentSegmentTree(i64 l, i64 r) : L(l), R(r), root(1, nullptr) {}
+	int set(int version, i64 i, S value) {
+		root.push_back(set(std::ssize(root), i, value, L, R, root[version]));
+		return std::ssize(root) - 1;
+	}
+	S get(int version, i64 i) { return get(i, L, R, root[version]); }
+	S sum(int version, i64 l, i64 r) { return sum(l, r, L, R, root[version]); }
+	i64 bisect(int version, auto pred) {
+		i64 result = R;
+		bisect(result, S{}, pred, L, R, root[version]);
+		return result;
+	}
+	int size() { return std::ssize(root) - 1; }
+	~PersistentSegmentTree() {
+		while (root.size()) {
+			delete root.back();
+			root.pop_back();
+		}
+	}
+};
+
+}; // namespace mo::internal
+
+#endif
