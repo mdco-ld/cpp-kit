3c954596fe8ab18fe815275a357d8e785e7c45a6
Miguel <migueldco14@gmail.com>
2025-08-07 18:12:02 -0300

    minor changes

--- include/mo/internal/persistent-segment-tree.hpp
+++ include/mo/internal/persistent-segment-tree.hpp
@@ -37,6 +37,7 @@ template <traits::Monoid S> class PersistentSegmentTree {
 		}
 		node->value = getValue(node->left) + getValue(node->right);
 	}
+
 	Node *set(int t, i64 i, S value, i64 tl, i64 tr, Node *node) {
 		Node *result = new Node();
 		result->t = t;
@@ -59,6 +60,31 @@ template <traits::Monoid S> class PersistentSegmentTree {
 		update(result);
 		return result;
 	}
+
+	Node *add(int t, i64 i, S value, i64 tl, i64 tr, Node *node) {
+		Node *result = new Node();
+		result->t = t;
+		if (node != nullptr) {
+			result->left = node->left;
+			result->right = node->right;
+			result->value = node->value;
+		}
+		if (tl == i && tr == i + 1) {
+			result->value += value;
+			return result;
+		}
+		i64 tm = tl + (tr - tl) / 2;
+		if (i < tm) {
+			result->left =
+				add(t, i, value, tl, tm, node ? node->left : nullptr);
+		} else {
+			result->right =
+				add(t, i, value, tm, tr, node ? node->right : nullptr);
+		}
+		update(result);
+		return result;
+	}
+
 	S get(i64 i, i64 tl, i64 tr, Node *&node) {
 		if (node == nullptr) {
 			return S{};
@@ -109,6 +135,10 @@ template <traits::Monoid S> class PersistentSegmentTree {
 	PersistentSegmentTree(i64 l, i64 r) : L(l), R(r), root(1, nullptr) {}
 	int set(int version, i64 i, S value) {
 		root.push_back(set(std::ssize(root), i, value, L, R, root[version]));
+		return std::ssize(root) - 1;
+	}
+	int add(int version, i64 i, S value) {
+		root.push_back(add(std::ssize(root), i, value, L, R, root[version]));
 		return std::ssize(root) - 1;
 	}
 	S get(int version, i64 i) { return get(i, L, R, root[version]); }
